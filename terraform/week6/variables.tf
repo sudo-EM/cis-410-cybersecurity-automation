6c5d013ef1b4af91ffdea40547b28ee870a371eb
Eli Medina <sudo-EM@users.noreply.github.com>
2026-05-12 11:40:20 -0700

    Week 6: First Terraform config — GCS bucket

---
+++ terraform/week6/variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+
+  description = "Your GCP Project ID"
+
+  type = string
+
+}
+
+variable "region" {
+
+  description = "Default GCP region for resources"
+
+  type = string
+
+  default = "us-central1"
+
+}
